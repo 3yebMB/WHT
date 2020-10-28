3e5ec258fd76cf3581139a2546b77e27b115cf0d
3yebmb <3yebmb@gmail.com>
2020-10-28 17:14:26 +0500

    add Retrofit

---
+++ app/src/main/java/dev/m13d/wht/domain/model/Holyday.kt
@@ -0,0 +1,13 @@
+package dev.m13d.wht.business.domain.model
+
+data class Holyday(
+    var date: String,
+    var localName: String,
+    var name: String,
+    var countryCode: String,
+    var fixed: Boolean,
+    var global: Boolean,
+    var counties: List<String>,
+    var launchYear: Int,
+    var type: String,
+)
